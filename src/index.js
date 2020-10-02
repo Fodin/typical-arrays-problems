3e2f9d7a95a0f84f211efa36ca78a9a9935bf8e4
Fedor Odintsov <fadmin@list.ru>
2020-10-03 02:38:19 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array === undefined || array.length === 0) {
+		return 0;
+	}
+	
+	let min = array[0];
+	
+	for (let el of array) {
+		if (el < min){
+			min = el;
+		}
+	}
+	return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array === undefined || array.length === 0) {
+		return 0;
+	}
+	
+	let max = array[0];
+	
+	for (let el of array) {
+		if (el > max){
+			max = el;
+		}
+	}
+	return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array === undefined || array.length === 0) {
+		return 0;
+	}
+	
+	return array.reduce((a, b) => (a + b)) / array.length;
 }
